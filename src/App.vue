7e28973419e1fc5372086a332002be724729b4a8
Satyabrat Jha <satyabrat.jha@vidyamantra.com>
2022-12-06 10:08:01 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import axios from 'axios'
+  import {ref} from 'vue'
+
+  const joke = ref('')
+
+//  using axios
+  // axios.get('https://api.chucknorris.io/jokes/random')
+  //   .then(res => {
+  //     joke.value = res.data.value
+  //   })
+
+// using try catch await
+  const jokeApi = async() => {
+    try {
+      const res = await axios.get('https://api.chucknorris.io/jokes/random')
+      joke.value = res.data.value
+    } catch(err) {
+      console.log(err)
+    }
+  }
+  jokeApi()
+
+</script>
+
+<template>
+  <h1>Hello</h1>
+  <h3>{{ joke }}</h3>
+</template>
+
+<style>
+
+</style>
